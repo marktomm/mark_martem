18f3fc0680963b56575214d289047415b8181c70
Mark Tomm <marktomm90@gmail.com>
2013-05-20 00:05:29 +0300

    minor adjustment in script1.sh

--- script1.sh
+++ script1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ "$1" = "" ]
 then
-TOTALRAM=$((`grep -i 'MemTotal' /proc/meminfo | awk '{print $2}'`/1000))
-FREEMEM=$((`grep -i 'MemFree' /proc/meminfo | awk '{print $2}'`/1000))
+TOTALRAM=$((`grep -i 'MemTotal' /proc/meminfo | awk '{print $2}' | sed s/...$//`))
+FREEMEM=$((`grep -i 'MemFree' /proc/meminfo | awk '{print $2}' | sed s/...$//`))
 USED=$((TOTALRAM - FREEMEM))
 printf "RAM: %d MB used / %d MB total ( %d MB free )\n" $USED $TOTALRAM $FREEMEM
 else
-TOTALRAM=$((`grep -i 'MemTotal' $1 | awk '{print $2}'`/1000))
-FREEMEM=$((`grep -i 'MemFree' $1 | awk '{print $2}'`/1000))
+TOTALRAM=$((`grep -i 'MemTotal' $1 | awk '{print $2}' | sed s/...$//`))
+FREEMEM=$((`grep -i 'MemFree' $1 | awk '{print $2}' | sed s/...$//`))
 USED=$((TOTALRAM - FREEMEM))
 printf "RAM: %d MB used / %d MB total ( %d MB free )\n" $USED $TOTALRAM $FREEMEM
 fi
